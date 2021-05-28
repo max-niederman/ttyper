65ffea739f781f8eef6caaaadef35451ec17c712
Max Niederman <max@maxniederman.com>
2021-05-27 23:10:59 -0700

    feat(contents): embed languages in binary and use as last resort

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,19 @@ use crossterm::{
 };
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use rust_embed::RustEmbed;
+use std::borrow::Cow;
 use std::fs;
 use std::io::{self, BufRead};
 use std::num;
 use std::path::PathBuf;
+use std::str;
 use structopt::StructOpt;
 use tui::{backend::CrosstermBackend, terminal::Terminal};
+
+#[derive(RustEmbed)]
+#[folder = "resources/runtime"]
+struct Resources;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "ttyper", about = "Terminal-based typing test.")]
@@ -37,7 +44,7 @@ struct Opt {
 }
 
 impl Opt {
-    fn gen_contents(&self) -> Vec<String> {
+    fn gen_contents(&self) -> Option<Vec<String>> {
         match &self.contents {
             Some(path) => {
                 let file = fs::File::open(path).expect("Error reading language file.");
@@ -45,34 +52,40 @@ impl Opt {
                     .lines()
                     .filter_map(Result::ok)
                     .collect();
-                lines.iter().map(String::from).collect()
+                Some(lines.iter().map(String::from).collect())
             }
             None => {
-                let language: Vec<String> = {
-                    let path = self.language_file.clone().unwrap_or_else(|| {
-                        dirs::config_dir()
-                            .expect("Couldn't find configuration directory.")
-                            .join("ttyper")
-                            .join("language")
-                            .join(&self.language)
-                    });
-                    let file = fs::File::open(path).expect("Error reading language file. Make sure Luthien's config dir isn't missing.");
-                    io::BufReader::new(file)
-                        .lines()
-                        .filter_map(Result::ok)
-                        .collect()
-                };
+                let bytes: Vec<u8> = self
+                    .language_file
+                    .clone()
+                    .or_else(|| {
+                        Some(
+                            dirs::config_dir()?
+                                .join("ttyper")
+                                .join("language")
+                                .join(&self.language),
+                        )
+                    })
+                    .and_then(|path| fs::read(path).ok())
+                    .or_else(|| {
+                        Resources::get(&format!("language/{}", self.language)).map(Cow::into_owned)
+                    })?;
+                let language: Vec<&str> = str::from_utf8(&bytes)
+                    .expect("Language file had non-utf8 encoding.")
+                    .lines()
+                    .collect();
 
                 let mut words: Vec<String> = language
                     .into_iter()
                     .cycle()
                     .take(self.words.into())
+                    .map(String::from)
                     .collect();
 
                 let mut rng = thread_rng();
                 words.shuffle(&mut rng);
 
-                words
+                Some(words)
             }
         }
     }
@@ -153,7 +166,9 @@ fn main() -> crossterm::Result<()> {
     let opt = Opt::from_args();
 
     loop {
-        let contents = opt.gen_contents();
+        let contents = opt.gen_contents().expect(
+            "Couldn't get test contents. Make sure the specified language actually exists.",
+        );
         if contents.is_empty() {
             println!("Test contents were empty. Exiting...");
             return Ok(());
