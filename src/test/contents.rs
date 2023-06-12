e4dde6c3747e63df308d74a8c2169a8faaa61c57
Max Niederman <max@maxniederman.com>
2023-06-12 09:33:34 -0700

    feat(test/contents): add char and grapheme iterators

--- src/test/contents.rs
+++ src/test/contents.rs
@@ -1,4 +1,5 @@
 use std::str::FromStr;
+use finl_unicode::grapheme_clusters::Graphemes;
 
 /// A trait for types that can be used as test contents.
 ///
@@ -15,12 +16,6 @@ pub enum Lexer {
     English,
 }
 
-impl Lexer {
-    fn consume_lexeme(&self, bytes: impl Iterator<Item = u8>) -> String {
-        todo!()
-    }
-}
-
 impl Default for Lexer {
     fn default() -> Self {
         Self::ExtendedGraphemeClusters
@@ -35,6 +30,47 @@ impl FromStr for Lexer {
             "english" => Ok(Self::English),
             "extended-grapheme-clusters" => Ok(Self::ExtendedGraphemeClusters),
             _ => Err("invalid lexer language"),
+        }
+    }
+}
+
+struct Utf8Chars<I> {
+    bytes: I,
+}
+
+impl<I> Iterator for Utf8Chars<I>
+where
+    I: Iterator<Item = u8>,
+{
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        const CONTINUATION_MASK: u8 = 0b0011_1111;
+
+        let first = self.bytes.next()?;
+        match first.leading_ones() {
+            0 => Some(char::from(first)),
+            1 => {
+                let first = first & 0b0001_1111;
+                let second = self.bytes.next()? & CONTINUATION_MASK;
+                char::from_u32((first as u32) << 6 | second as u32)
+            }
+            2 => {
+                let first = first & 0b0000_1111;
+                let second = self.bytes.next()? & CONTINUATION_MASK;
+                let third = self.bytes.next()? & CONTINUATION_MASK;
+                char::from_u32((first as u32) << 12 | (second as u32) << 6 | third as u32)
+            }
+            3 => {
+                let first = first & 0b0000_0111;
+                let second = self.bytes.next()? & CONTINUATION_MASK;
+                let third = self.bytes.next()? & CONTINUATION_MASK;
+                let fourth = self.bytes.next()? & CONTINUATION_MASK;
+                char::from_u32(
+                    (first as u32) << 18 | (second as u32) << 12 | (third as u32) << 6 | fourth as u32,
+                )
+            }
+            _ => panic!("invalid UTF-8"),
         }
     }
 }
