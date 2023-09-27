7f6b2df719350f435ffdfef6c0346cf15c527526
Max Niederman <max@maxniederman.com>
2023-09-26 21:29:58 -0700

    fix history implementation

--- src/trial/history.rs
+++ src/trial/history.rs
@@ -1,4 +1,4 @@
-use std::{iter, ops::Range};
+use std::{collections::btree_map::Entry, convert::TryFrom, iter, ops::Range};
 
 use unicode_segmentation::UnicodeSegmentation;
 
@@ -86,59 +86,38 @@ impl History {
         // The index of the new column.
         let col = self.reference_indices.len();
 
-        // The index of the row being updated.
-        let mut row = col.saturating_sub(self.max_misalignment);
+        // A peekable iterator over indices of the previous column.
+        // Used to get the left and top-left neighbors.
+        let mut prev_col_indices = self.col_indices(col - 1).peekable();
 
-        // None of the entries in the row are tracked.
-        if row > self.typed_indices.len() {
-            return;
-        }
-
-        // The index in the tracked entry array of the current entry.
-        let mut idx = {
-            let row_start = row * self.tracked_entries_per_row();
-            let offset = col + self.max_misalignment - row;
-            row_start + offset
-        };
         // The value of the top neighbor.
         // I.e., the last entry entered.
         let mut top = u32::MAX;
-        // The value of the top-left neighbor.
-        // I.e., the last entry's left neighbor.
-        let mut top_left = idx
-            .checked_sub(self.tracked_entries_per_row())
-            .map(|i| self.nw_entries[i])
-            .unwrap_or(u32::MAX);
 
-        loop {
-            let replacement_cost =
-                top_left.saturating_add(self.replacement_cost(row, col).unwrap_or(u32::MAX));
-            let deletion_cost = top.saturating_add(Self::DELETION_COST);
-
-            // This is the last tracked entry.
-            if col + self.max_misalignment == row {
-                self.nw_entries[idx] = *[replacement_cost, deletion_cost].iter().min().unwrap();
-                break;
+        for EntryIndices { row, buf: idx, .. } in self.col_indices(col) {
+            let top_left = if row <= 0 {
+                u32::MAX
             } else {
-                let left = self.nw_entries[idx - 1];
-                let insertion_cost = left.saturating_add(Self::INSERTION_COST);
+                self.nw_entries[prev_col_indices.next().unwrap().buf]
+            };
+            let left = prev_col_indices
+                .peek()
+                .map(|&i| self.nw_entries[i.buf])
+                .unwrap_or(u32::MAX);
 
-                let val = *[deletion_cost, replacement_cost, insertion_cost]
-                    .iter()
-                    .min()
-                    .unwrap();
-                self.nw_entries[idx] = val;
+            let replacement_cost = top_left
+                .saturating_add(self.replacement_cost(row, col as isize).unwrap_or(u32::MAX));
+            let insertion_cost = top.saturating_add(Self::INSERTION_COST);
+            let deletion_cost = left.saturating_add(Self::DELETION_COST);
 
-                row += 1;
-                idx += self.tracked_entries_per_row() - 1;
+            let val = *[replacement_cost, insertion_cost, deletion_cost]
+                .iter()
+                .min()
+                .unwrap();
 
-                if idx >= self.nw_entries.len() {
-                    break;
-                }
+            self.nw_entries[idx] = val;
 
-                top = val;
-                top_left = left;
-            }
+            top = val;
         }
     }
 
@@ -160,29 +139,33 @@ impl History {
             return;
         }
 
-        // The current column.
-        // Very high values are outside the reference string.
-        let mut col = row.wrapping_sub(self.max_misalignment);
-
         // A peekable iterator over indices of the previous row.
         // Used to get the top and top-left neighbors.
         let mut prev_row_indices = self.row_indices(row - 1).peekable();
         // The value of the left neighbor.
         let mut left = u32::MAX;
 
-        loop {
+        self.nw_entries.resize(
+            self.nw_entries.len() + self.tracked_entries_per_row(),
+            u32::MAX,
+        );
+
+        for EntryIndices { col, buf: idx, .. } in self.row_indices(row).filter({
+            let cols = self.reference_indices.len() + 1;
+            move |&i| i.col < cols as isize
+        }) {
             let top_left = match prev_row_indices.next() {
-                Some(i) => self.nw_entries[i],
+                Some(i) => self.nw_entries[i.buf],
                 None => break,
             };
 
             let top = prev_row_indices
                 .peek()
-                .map(|&i| self.nw_entries[i])
+                .map(|&i| self.nw_entries[i.buf])
                 .unwrap_or(u32::MAX);
 
-            let replacement_cost =
-                top_left.saturating_add(self.replacement_cost(row, col).unwrap_or(u32::MAX));
+            let replacement_cost = top_left
+                .saturating_add(self.replacement_cost(row as isize, col).unwrap_or(u32::MAX));
             let insertion_cost = top.saturating_add(Self::DELETION_COST);
             let deletion_cost = left.saturating_add(Self::INSERTION_COST);
 
@@ -191,9 +174,7 @@ impl History {
                 .min()
                 .unwrap();
 
-            self.nw_entries.push(val);
-
-            col = col.wrapping_add(1);
+            self.nw_entries[idx] = val;
 
             left = val;
         }
@@ -213,28 +194,52 @@ impl History {
         2 * self.max_misalignment + 1
     }
 
-    fn row_indices(&self, row: usize) -> Range<usize> {
-        let row_start = row * self.tracked_entries_per_row();
-        row_start..row_start + self.tracked_entries_per_row()
+    fn row_indices(&self, row: usize) -> impl Iterator<Item = EntryIndices> {
+        let first_col = row as isize - self.max_misalignment as isize;
+
+        let first_buf = row * self.tracked_entries_per_row();
+        let buf_range = first_buf..first_buf + self.tracked_entries_per_row();
+
+        buf_range
+            .zip(first_col..)
+            .map(move |(buf, col)| EntryIndices {
+                row: row as isize,
+                col,
+                buf,
+            })
     }
 
-    fn col_indices(&self, col: usize) -> impl Iterator<Item = usize> {
+    fn col_indices(&self, col: usize) -> impl Iterator<Item = EntryIndices> {
         let tracked_entries_per_row = self.tracked_entries_per_row();
         let nw_entries_len = self.nw_entries.len();
 
-        let first =
-            (col.saturating_sub(self.max_misalignment) + 1) * self.tracked_entries_per_row() - 1;
+        let first_row = col.saturating_sub(self.max_misalignment) as isize;
+        let buf = if first_row == 0 {
+            self.max_misalignment + col as usize
+        } else {
+            (first_row as usize + 1) * tracked_entries_per_row - 1
+        };
 
-        iter::successors(Some(first), move |&i| {
-            i.checked_add(tracked_entries_per_row - 1)
+        let first = EntryIndices {
+            row: first_row,
+            col: col as isize,
+            buf,
+        };
+
+        iter::successors(Some(first), move |&EntryIndices { row, col, buf }| {
+            Some(EntryIndices {
+                row: row + 1,
+                col,
+                buf: buf + tracked_entries_per_row - 1,
+            })
         })
-        .take_while(move |&i| i < nw_entries_len)
+        .take_while(move |&idx| idx.buf < nw_entries_len)
         .take(tracked_entries_per_row)
     }
 
-    fn replacement_cost(&self, row: usize, col: usize) -> Option<u32> {
-        let typed_bidx = *self.typed_indices.get(row.checked_sub(1)?)?;
-        let reference_bidx = *self.reference_indices.get(col.checked_sub(1)?)?;
+    fn replacement_cost(&self, row: isize, col: isize) -> Option<u32> {
+        let typed_bidx = *self.typed_indices.get(usize::try_from(row - 1).ok()?)?;
+        let reference_bidx = *self.reference_indices.get(usize::try_from(col - 1).ok()?)?;
 
         let typed = first_grapheme_cluster(&self.typed[typed_bidx..]);
         let reference = first_grapheme_cluster(&self.reference[reference_bidx..]);
@@ -247,12 +252,22 @@ impl History {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct EntryIndices {
+    row: isize,
+    col: isize,
+    buf: usize,
+}
+
 fn first_grapheme_cluster(string: &str) -> &str {
     string.graphemes(true).next().unwrap()
 }
 
 #[cfg(test)]
 mod tests {
+    use rand::{seq::SliceRandom, SeedableRng};
+    use rand_chacha::ChaCha20Rng;
+
     use super::*;
 
     #[test]
@@ -268,10 +283,8 @@ mod tests {
     }
 
     #[test]
-    fn push_saturday_sunday() {
-        let mut history = History::new(2);
-
-        let nw_mat = [
+    fn replicates_needleman_wunsch_with_high_max_misalignment() {
+        const NW_MAT: [[u32; 9]; 7] = [
             [0, 1, 2, 3, 4, 5, 6, 7, 8],
             [1, 0, 1, 2, 3, 4, 5, 6, 7],
             [2, 1, 2, 3, 2, 3, 4, 5, 6],
@@ -280,21 +293,32 @@ mod tests {
             [5, 4, 3, 4, 5, 6, 5, 4, 5],
             [6, 5, 4, 5, 6, 7, 6, 5, 4],
         ];
+        const REFERENCE: &'static str = "saturday";
+        const TYPED: &'static str = "sunday";
+        const PERMUTATIONS: usize = 1_000;
 
-        history.push_reference("s");
-        check_tracked_rows(&history, &nw_mat);
+        let mut rng = ChaCha20Rng::seed_from_u64(42);
 
-        history.push_reference("a");
-        check_tracked_rows(&history, &nw_mat);
+        let mut push_order = [vec![true; 6], vec![false; 8]].concat();
 
-        history.push_reference("t");
-        check_tracked_rows(&history, &nw_mat);
+        for _ in 0..PERMUTATIONS {
+            push_order.shuffle(&mut rng);
 
-        history.push_typed("s");
-        check_tracked_rows(&history, &nw_mat);
+            let mut history = History::new(8);
 
-        history.push_typed("u");
-        check_tracked_rows(&history, &nw_mat);
+            let mut typed = TYPED.graphemes(true);
+            let mut reference = REFERENCE.graphemes(true);
+
+            for &push_typed in &push_order {
+                if push_typed {
+                    history.push_typed(typed.next().unwrap());
+                } else {
+                    history.push_reference(reference.next().unwrap());
+                }
+
+                check_tracked_rows(&history, &NW_MAT);
+            }
+        }
     }
 
     fn check_tracked_rows<const M: usize, const N: usize>(
