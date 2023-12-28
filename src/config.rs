46cbfc211ee90da695f59d1ca710a3a2409ebcdc
Pravith <pravithba10@gmail.com>
2023-12-28 20:06:38 +0530

    Feature: Added basic configs.

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,98 @@
+use crossterm::event::{KeyCode, KeyModifiers};
 use ratatui::style::{Color, Modifier, Style};
 use serde::{
     de::{self, IntoDeserializer},
     Deserialize,
 };
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
 #[serde(default)]
 pub struct Config {
     pub default_language: String,
     pub theme: Theme,
+    pub key_map: KeyMap,
 }
 
-impl Default for Config {
+#[derive(Debug, Deserialize, Default)]
+#[serde(default)]
+pub struct KeyMap {
+    #[serde(deserialize_with = "deseralize_key")]
+    pub remove_previous_word: Key,
+}
+
+fn deseralize_key<'de, D>(deserializer: D) -> Result<Key, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    struct KeyVisitor;
+    impl<'de> de::Visitor<'de> for KeyVisitor {
+        type Value = Key;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            let mut key = Key {
+                code: KeyCode::Null,
+                modifier: Some(KeyModifiers::NONE),
+            };
+            match v.split('-').count() {
+                2 => {
+                    let mut split = v.split('-');
+                    let key_code = split.next();
+                    if let Some(key_code) = key_code {
+                        if key_code.len() == 1 {
+                            let key_code_char = key_code.chars().next();
+                            if let Some(key_code_char) = key_code_char {
+                                match key_code_char {
+                                    'C' => {
+                                        key.modifier = Some(KeyModifiers::CONTROL);
+                                    }
+                                    'A' => {
+                                        key.modifier = Some(KeyModifiers::ALT);
+                                    }
+                                    _ => {}
+                                }
+                            }
+                        }
+                    }
+                    let key_code = split.next();
+                    if let Some(key_code) = key_code {
+                        if key_code.len() == 1 {
+                            let key_code_char = key_code.chars().next();
+                            if let Some(key_code_char) = key_code_char {
+                                if key_code_char.is_lowercase() {
+                                    key.code = KeyCode::Char(key_code_char)
+                                }
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+
+            Ok(key)
+        }
+    }
+
+    return deserializer.deserialize_str(KeyVisitor);
+}
+
+#[derive(Debug)]
+pub struct Key {
+    pub code: KeyCode,
+    pub modifier: Option<KeyModifiers>,
+}
+
+impl Default for Key {
     fn default() -> Self {
         Self {
-            default_language: "english200".into(),
-            theme: Theme::default(),
+            code: KeyCode::Null,
+            modifier: Some(KeyModifiers::NONE),
         }
     }
 }
